0d7ea3e1eb3c016e4fb4ec5bfedf43a9146f8a01
Mia Pan <miapanyuewei@gmail.com>
2019-10-14 00:07:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end 
-def introduction(name,language)
+def introduction with (name,language)
   puts" Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
